8b0be00cc1664dbbd69388432e83c85b41caea21
Blake Wang <bwang@intouchhealth.com>
2020-10-15 10:58:15 -0700

    Get the progress bars working!

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,78 @@
 import React from 'react'
 import Step from './Step'
 
+import 'regenerator-runtime/runtime'
+
 import '../styles/styles.scss'
 
 class App extends React.Component {
   state = {
-    now: 69
+    step: 0,
+    now: 0
+  }
+
+  calcNow = (step) => {
+    if (this.state.step > step) {
+      return 100
+    } else if (this.state.step < step) {
+      return 0
+    } else {
+      return this.state.now
+    }
+  }
+
+  sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  updateStepAndNow = async () => {
+    const period5 = 0.5 * 60 * 1000 / 100
+    const period2 = 0.2 * 60 * 1000 / 100
+
+    for (let i = 0; i < 699; i++) {
+      const period = i < 99 ? period5 : period2
+      const step = ~~(i / 100)
+      const now = i % 100
+
+      await new Promise(resolve => setTimeout(resolve, period))
+      this.setState(() => ({ step, now }))
+    }
+  }
+
+  componentDidMount () {
+    this.updateStepAndNow()
   }
 
   render () {
     return (
       <div className='container'>
         <Step
-          letter='S'
-          description='Stop - 5min'
-          now={this.state.now}
+          label='Stop - 5min'
+          now={this.calcNow(0)}
+        />
+        <Step
+          label='Increase volume - 2min'
+          now={this.calcNow(1)}
+        />
+        <Step
+          label='Touch - 2min'
+          now={this.calcNow(2)}
+        />
+        <Step
+          label='Binky - 2min'
+          now={this.calcNow(3)}
+        />
+        <Step
+          label='Add rocking - 2min'
+          now={this.calcNow(4)}
+        />
+        <Step
+          label='Cuddle - 2min'
+          now={this.calcNow(5)}
+        />
+        <Step
+          label='K, time to feed!'
+          now={this.calcNow(6)}
         />
       </div>
     )
--- src/components/Step.js
+++ src/components/Step.js
@@ -3,10 +3,8 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 
 const Step = (props) => (
   <div className='row'>
-    <div className='col-sm-1'>{props.letter}</div>
-    <div className='col-sm-11'>{props.description}</div>
     <div className='col-sm-12'>
-      <ProgressBar now={props.now} label={`${props.now}%`} />
+      <ProgressBar now={props.now} label={`${props.label} (${props.now}%)`} />
     </div>
   </div>
 )
