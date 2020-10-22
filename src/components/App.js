c0f47b5a4d9c3f1f74ae38df88d7e39707d70385
Blake Wang <bwang@intouchhealth.com>
2020-10-22 12:27:10 -0700

    Get it working with individual clocks

--- src/components/App.js
+++ src/components/App.js
@@ -3,30 +3,13 @@ import Step from './Step'
 
 import 'regenerator-runtime/runtime'
 
-const App = (props) => {
+const App = () => {
   const [progressBarHeight, setProgressBarHeight] = useState('2rem')
-  const [timeIndex, setTimeIndex] = useState(0)
-
-  const initTime = async () => {
-    const period5 = 5 * 60 * 1000 / 100
-    const period2 = 2 * 60 * 1000 / 100
-
-    for (let i = 0; i < 699; i++) {
-      console.log(`loop ${i}`)
-      const period = i < 99 ? period5 : period2
-      await new Promise(resolve => setTimeout(resolve, period))
-
-      setTimeIndex(i)
-    }
-  }
-
-  const calcProgressBarHeight = () => (
-    `${((window.innerHeight - 85) / 7 - 53) / 10}rem`
-  )
 
   useEffect(() => {
-    initTime()
-    setProgressBarHeight(calcProgressBarHeight())
+    setProgressBarHeight(
+      `${((window.innerHeight - 85) / 7 - 53) / 10}rem`
+    )
   })
 
   return (
@@ -34,43 +17,36 @@ const App = (props) => {
       <h1>sitback</h1>
       <Step
         label='Stop - 5min'
-        timeIndex={timeIndex}
         stepIndex={0}
         progressBarHeight={progressBarHeight}
       />
       <Step
         label='Increase volume - 2min'
-        timeIndex={timeIndex}
         stepIndex={1}
         progressBarHeight={progressBarHeight}
       />
       <Step
         label='Touch - 2min'
-        timeIndex={timeIndex}
         stepIndex={2}
         progressBarHeight={progressBarHeight}
       />
       <Step
         label='Binky - 2min'
-        timeIndex={timeIndex}
         stepIndex={3}
         progressBarHeight={progressBarHeight}
       />
       <Step
         label='Add rocking - 2min'
-        timeIndex={timeIndex}
         stepIndex={4}
         progressBarHeight={progressBarHeight}
       />
       <Step
         label='Cuddle - 2min'
-        timeIndex={timeIndex}
         stepIndex={5}
         progressBarHeight={progressBarHeight}
       />
       <Step
         label='K, time to feed!'
-        timeIndex={timeIndex}
         stepIndex={6}
         progressBarHeight={progressBarHeight}
       />
--- src/components/Step.js
+++ src/components/Step.js
@@ -1,33 +1,47 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ProgressBar from 'react-bootstrap/ProgressBar'
 
-const Step = (props) => {
-  const step = ~~(props.timeIndex / 100)
-  let now
+const Step = ({ label, stepIndex, progressBarHeight }) => {
+  const [now, setNow] = useState(0)
 
-  if (step < props.stepIndex) {
-    now = 0
-  } else if (step > props.stepIndex) {
-    now = 100
-  } else {
-    now = props.timeIndex % 100
-  }
+  const firstStepMinutes = 5
+  const otherStepMinutes = 2
+  const minutes = stepIndex ? firstStepMinutes : otherStepMinutes
+  const timeout = stepIndex ?
+    (firstStepMinutes + otherStepMinutes * (stepIndex - 1)) * 60000
+    :
+    0
+  const interval = minutes * 60000 / 100
+
+  useEffect(() => {
+    const startProgress = () => {
+      let i = 0
+      const intervalRef = setInterval(() => {
+        setNow(prevNow => prevNow + 1)
+        if (++i == 100) {
+          clearInterval(intervalRef)
+        }
+      }, interval)
+    }
+
+    setTimeout(startProgress, timeout)
+  }, [])
 
   return (
     <div>
       <div className='row'>
         <div className='col-sm-12'>
           <label className='progress-label'>
-            {`${props.label} (${now}%)`}
+            {`${label} (${now}%)`}
           </label>
         </div>
       </div>
       <div className='row'>
         <div className='col-sm-12'>
           <ProgressBar
-            style={{ height: props.progressBarHeight }}
+            style={{ height: progressBarHeight }}
             now={now}
-            variant={`num-${props.stepIndex}`}
+            variant={`num-${stepIndex}`}
           />
         </div>
       </div>
